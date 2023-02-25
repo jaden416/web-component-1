c2b0e200f81d28b718adca3e1213f7a1c850db7e
Jaden Lewis-Chambers <you@example.com>
2023-02-24 20:09:21 -0500

    added index and naming

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,79 +1,25 @@
 import gsap, { Draggable } from 'gsap/all'
 gsap.registerPlugin(Draggable)
 
+
 // This is a web component, it functions like a React component but it is made using vanilla javascript.
 export default class Card extends HTMLElement {
+
+  
   constructor(card){
     super();
     this.attachShadow({ mode: "open"});
 
     this.card = card
     this.images = card.images
-
+    this.name = card.name
   }
   
+
+
   // Renders the card 
  connectedCallback() {
-  this.slides = 0
-
   this.render();
-  this.prevButton = this.shadowRoot.querySelector('.card__prev')
-  this.nextButton = this.shadowRoot.querySelector('.card__next')
-  
-  this.gallery = this.shadowRoot.querySelector('.card__media__gallery')
-  this.carousel = this.shadowRoot.querySelector('.card__media__carousel')
-  this.slideArray  = this.shadowRoot.querySelectorAll('.card__image__wrapper')
-  const self = this
-
-
-  Draggable.create(this.carousel,{
-    type: 'x',
-    inertia: 'true',
-    direction: "right",
-    throwProps: true,
-    resistance: 1000,
-    onDragEnd: function() {
-
-
-      let nearest100 = Math.round(calcPercent(this.x) / 100) * 100;
-      function calcPercent(x) {
-        const Xpercent = Math.round(x / self.carousel.offsetWidth * 100);
-        return Xpercent;
-      }
-
-      // if you are scrolling to the left on the first card
-      if (nearest100 > 0) {
-        nearest100 = 0;
-      }// if you are scrolling to the right on the last card
-      else if (nearest100 <= self.carousel.childElementCount * -100){
-        nearest100 = `-${self.carousel.children.length -1}00`
-      }
-
-      gsap.to(this.target, 
-        { 
-          x: `${nearest100}%`, 
-          duration: 0.3, 
-          ease: 'power2.out', 
-          onComplete: () => {
-          self.slides = Math.abs(nearest100 / 100);
-          console.log('yoo', self.slides)
-          if (self.slides === 0){
-            self.prevButton.style.display = 'none';
-          }else{
-            self.prevButton.style.display = 'flex';
-          }
-
-          if (self.slides === self.carousel.childElementCount -1){
-            self.nextButton.style.display = 'none';
-          }else{
-            self.nextButton.style.display = 'flex';
-          }
-          
-          } 
-        })
-
-    }
-  });
 }
 
 
@@ -98,6 +44,7 @@ export default class Card extends HTMLElement {
           ease: 'power2.out', 
           onComplete: () => {
             this.onToggle();
+            this.update()
           } 
         });
     }
@@ -117,6 +64,8 @@ export default class Card extends HTMLElement {
           ease: 'power2.out', 
           onComplete: () => {
             this.onToggle();
+            this.update()
+
           } 
         });
     }
@@ -155,20 +104,92 @@ export default class Card extends HTMLElement {
     this.nextButton.style.opacity = '0'
     this.nextButton.style.transform = 'translate3d(0px, 20px, 0px)'
   }
+
+  onDrag(){
+    const self =this
+
+    this.slides = 0
+    this.prevButton = this.shadowRoot.querySelector('.card__prev')
+    this.nextButton = this.shadowRoot.querySelector('.card__next')
+    this.gallery = this.shadowRoot.querySelector('.card__media__gallery')
+    this.carousel = this.shadowRoot.querySelector('.card__media__carousel')
+    this.slideArray  = this.shadowRoot.querySelectorAll('.card__image__wrapper')
+
+    Draggable.create(this.carousel,{
+      type: 'x',
+      inertia: 'true',
+      direction: "right",
+      throwProps: true,
+      resistance: 1000,
+      onDragEnd: function() {
+  
+        let nearest100 = Math.round(calcPercent(this.x) / 100) * 100;
+        function calcPercent(x) {
+          const Xpercent = Math.round(x / self.carousel.offsetWidth * 100);
+          return Xpercent;
+        }
+  
+        // if you are scrolling to the left on the first card
+        if (nearest100 > 0) {
+          nearest100 = 0;
+        }// if you are scrolling to the right on the last card
+        else if (nearest100 <= self.carousel.childElementCount * -100){
+          nearest100 = `-${self.carousel.children.length -1}00`
+        }
+  
+        gsap.to(this.target, 
+          { 
+            x: `${nearest100}%`, 
+            duration: 0.3, 
+            ease: 'power2.out', 
+            onComplete: () => {
+            self.slides = Math.abs(nearest100 / 100);
+            if (self.slides === 0){
+              self.prevButton.style.display = 'none';
+            }else{
+              self.prevButton.style.display = 'flex';
+            }
+  
+            if (self.slides === self.carousel.childElementCount -1){
+              self.nextButton.style.display = 'none';
+            }else{
+              self.nextButton.style.display = 'flex';
+            }
+
+            self.update()
+            } 
+
+          })
+  
+      }
+    });
+  }
   
   addEventListeners(){
     this.shadowRoot.querySelector('.card__prev').addEventListener('click', (e) => this.buttonPrev(e));
     this.shadowRoot.querySelector('.card__next').addEventListener('click', (e) => this.buttonNext(e));
     this.shadowRoot.querySelector('.card__media').addEventListener('mouseover', () => this.onHover());
     this.shadowRoot.querySelector('.card__media').addEventListener('mouseleave', () => this.onLeave());
+    this.onDrag()
+
+
   }
   
-  
+  update(){
+
+    const slideIndexElement = this.shadowRoot.querySelector('.card__index__span');
+    slideIndexElement.innerHTML = `${this.slides+ 1} / ${this.carousel.childElementCount}`;
+  }
+
   render() { 
-    const template = `
+    
+
+    this.shadowRoot.innerHTML = (`
+
     ${this.style}
-    <a href="/" class= "card">
-        <div class="card__wrapper">
+
+    <a href="/committee/" class= "card">
+      <div class="card__wrapper">
         <div class="card__media">
           <button class="card__prev">
             <svg class="card__prev__icon" width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -182,21 +203,30 @@ export default class Card extends HTMLElement {
           </button>
           <div class="card__media__gallery">
             <div class="card__media__carousel">
-              ${this.images.map(image => `
-              <div class="card__image__wrapper">
-                <img class="card__image" src = ${image.src} alt=${image.alt} onmousedown="event.preventDefault()">
-              </div>
-              `).join('')}
+            ${this.images.map(image => `
+            <div class="card__image__wrapper">
+              <img class="card__image" src = ${image.src} alt=${image.alt} onmousedown="event.preventDefault()">
+            </div>
+            `).join('')}
+
             </div>
           </div>
         </div>
+        <div class="card__description">
+          <header class="card__description__header">
+            <p class="card__title">${this.name}</p>
+            <div class="card__index">
+              <span class="card__index__span">
+               1 / ${this.images.length}
+              </span>
+            </div>
+          </header>
         </div>
+      </div>
     </a>
   `
-  
-  this.shadowRoot.innerHTML = template;
-
-  this.addEventListeners()
+  )
+    this.addEventListeners()
   }
 
 }
@@ -206,3 +236,10 @@ export default class Card extends HTMLElement {
 if ('customElements' in window) {
 	window.customElements.define('home-card', Card);
 }
+
+
+// ${this.images.map(image => `
+// <div class="card__image__wrapper">
+//   <img class="card__image" src = ${image.url} alt=${image.alt} onmousedown="event.preventDefault()">
+// </div>
+// `).join('')}
--- app/data/cards.js
+++ app/data/cards.js
@@ -18,7 +18,7 @@ export const cards =  [
       {src: 'https://cdn.sanity.io/images/2q93byor/production/31e436716697f17694b7868b57827307f0c4c41f-2500x2269.jpg?w=800&auto=format', alt: 'some picture'}, 
       {src: 'https://cdn.sanity.io/images/2q93byor/production/8a0c3e3ff2068e62229ae93ce30eea9f5a8af0f3-2500x2258.jpg?w=800&auto=format', alt: 'some picture'},
     ],
-    name: ''
+    name: 'Styling Committee'
     
   },
   { 
@@ -29,7 +29,7 @@ export const cards =  [
       {src: 'https://cdn.sanity.io/images/2q93byor/production/466b37ffa5b52ad53a028bd61bf2f53d41d2b169-1250x1745.jpg?w=800&auto=format', alt: 'some picture'},
       {src: 'https://cdn.sanity.io/images/2q93byor/production/f6888dc9b9ebe4cc89e4d80079849352d244464a-609x800.gif?w=800&auto=format', alt: 'some picture'},
     ],
-    name: ''
+    name: 'Photography Committee'
     
   },
   { 
@@ -39,7 +39,7 @@ export const cards =  [
       {src: 'https://cdn.sanity.io/images/2q93byor/production/62a1145edbce1f4a79fd85fde3d0871a4f27b78e-4000x4000.jpg?w=800&auto=format', alt: 'some picture'}, 
       {src: 'https://cdn.sanity.io/images/2q93byor/production/a26d79830e94e26cc00b1c26d0741a1694da775a-4000x4000.jpg?w=800&auto=format', alt: 'some picture'},
     ],
-    name: ''
+    name: 'Beauty Committee'
   },
   { 
     id: 5,
@@ -48,7 +48,7 @@ export const cards =  [
       {src: 'https://cdn.sanity.io/images/2q93byor/production/7ac19e2a929ba8dcd69a526142b0ce162d023d30-2215x3000.jpg?w=800&auto=format', alt: 'some picture'}, 
       {src: 'https://cdn.sanity.io/images/2q93byor/production/558e55247fd54374077928d157272e305862cb11-2215x3000.jpg?w=800&auto=format', alt: 'some picture'},
     ],
-    name: ''
+    name: 'Production Committee'
   },
   { 
     id: 6,
@@ -59,7 +59,7 @@ export const cards =  [
       {src: 'https://cdn.sanity.io/images/2q93byor/production/3fad2f9a9a7a6b1251b4fd5dc7b1ed1245cdb494-2312x2312.jpg?w=800&auto=format', alt: 'some picture'},
       {src: 'https://cdn.sanity.io/images/2q93byor/production/d6f2b42ea37704baba183c7333e88374531b2172-2312x2312.jpg?w=800&auto=format', alt: 'some picture'},
     ],
-    name: ''
+    name: 'Social Media Committee'
   },
   { 
     id: 7,
@@ -68,7 +68,7 @@ export const cards =  [
       {src: 'https://cdn.sanity.io/images/2q93byor/production/02970e9e43349a7b3b3ab598e87fd9fad69a5861-2099x2099.gif?w=800&auto=format', alt: 'some picture'}, 
       {src: 'https://cdn.sanity.io/images/2q93byor/production/fdb97db6f6a03698d9aeb185a5fedf924bef77b8-4000x4000.jpg?w=800&auto=format', alt: 'some picture'},
     ],
-    name: ''
+    name: 'Web Development Committee'
   },
   { 
     id: 8,
@@ -77,6 +77,6 @@ export const cards =  [
       {src: 'https://cdn.sanity.io/images/2q93byor/production/763dad7c0b9b903c5ce0c44948298df4320b7270-2312x2312.jpg?w=800&auto=format', alt: 'some picture'}, 
       {src: 'https://cdn.sanity.io/images/2q93byor/production/33adddb974a633e07e148b5ebc6b46ad1515eba0-2312x2312.jpg?w=800&auto=format', alt: 'some picture'},
     ],
-    name: ''
+    name: 'Modeling Committee'
   },
 ]
